6cfd33dcc745c7e272649176b1a5173bda3919c0
unknown <bhuvaneswari@moonraft.com>
2020-10-23 13:06:17 +0530

    folder added

--- scripts/cart.js
+++ scripts/cart.js
@@ -7,10 +7,10 @@ xmlhttp.onreadystatechange = function () {
     data.items.forEach((element) => {
       html =
         html +
-        `<div class = 'card-container'><div  class = 'image-container'><img src ="../images/${element.image}" ></div><div class = "productDetailsContainer"><div class = "product-name">${element.name}</div><div class = "detailsContainer"><span style='color:red;text-decoration:line-through'><span style='color:black'>${element.price.display}</span></span><span>${element.price.actual}</span><button>Add to cart</button></div></div></div>`;
+        `<div class = 'card-container'><div  class = 'image-container'><img src ="./images/${element.image}" ></div><div class = "productDetailsContainer"><div class = "product-name">${element.name}</div><div class = "detailsContainer"><span style='color:red;text-decoration:line-through'><span style='color:black'>${element.price.display}</span></span><span>${element.price.actual}</span><button>Add to cart</button></div></div></div>`;
     });
     $(".itemsContainer").html(html);
   }
 };
-xmlhttp.open("GET", "../JsonData/cartData.json", true);
+xmlhttp.open("GET", "./JsonData/cartData.json", true);
 xmlhttp.send();
