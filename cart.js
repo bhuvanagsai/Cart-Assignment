a4732707169ffe6ac16c2dfdca59ed4d13f7304b
unknown <bhuvaneswari@moonraft.com>
2020-10-23 11:15:39 +0530

    added Card Folder

---
+++ cart.js
@@ -0,0 +1,16 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    $(".itemsContainer").html("");
+    var html = "";
+    data.items.forEach((element) => {
+      html =
+        html +
+        `<div class = 'card-container'><div  class = 'image-container'><img src ="./images/${element.image}" ></div><div class = "productDetailsContainer"><div class = "product-name">${element.name}</div><div class = "detailsContainer"><span style='color:red;text-decoration:line-through'><span style='color:black'>${element.price.display}</span></span><span>${element.price.actual}</span><button>Add to cart</button></div></div></div>`;
+    });
+    $(".itemsContainer").html(html);
+  }
+};
+xmlhttp.open("GET", "cartData.json", true);
+xmlhttp.send();
